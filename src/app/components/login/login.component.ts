0d2b110205e339226c0837591787a4bdb4d2de13
trilgar <zaritkiy.alexey@gmail.com>
2021-05-22 23:58:29 +0300

    hotfix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -56,7 +56,7 @@ export class LoginComponent implements OnInit {
           break;
         }
         default: {
-          this.warningMessage = 'unexpected error';
+          this.warningMessage = error;
           this.warningFlag = true;
           break;
         }
